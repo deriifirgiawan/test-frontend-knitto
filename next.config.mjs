7d4d565d183e93162216b371a2bfb991773bc971
deriifirgiawan <derifirgiawan025@gmail.com>
2024-01-29 02:57:26 +0700

    feat: add ISR config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+	reactStrictMode: true,
+	rewrites: async () => {
+		return [
+			{
+				source: "/api/:path",
+				destination: "https://jsonplaceholder.typicode.com/:path",
+			},
+		];
+	},
 };
 
 export default nextConfig;
